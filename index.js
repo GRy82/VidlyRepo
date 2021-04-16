7e6b783283d1db553e933214b048f7614311f814
GRy82 <gwryan@buffalo.edu>
2021-04-16 14:30:28 -0400

    express get endpoints set up for '/', '/api/genres', and '/api/genres/:id'

--- index.js
+++ index.js
@@ -6,5 +6,20 @@ const genres = [
     { id: 1, genre: 'sample-genre' }
 ];
 
+app.get('/', (req, res) => {
+    res.send('Welcome to Vidly! Try this endpoint: \'/api/genres\' ');
+});
+
+app.get('/api/genres', (req, res) => {
+    res.send(genres);
+});
+
+app.get('/api/genres/:id', (req, res) => {
+    const genre = genres.find(c => c.id === parseInt(req.params.id));
+    if(!genre) return res.status(404).send('genre not found.');
+
+    res.send(genre);
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
