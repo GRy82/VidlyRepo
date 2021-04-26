fd1e4d054033bf780af49e2a0850747598437a59
GRy82 <gwryan@buffalo.edu>
2021-04-26 10:04:33 -0400

    Some changes made to the way the post and put route handlers are conducted.  instead of setting genre subdocument equal to the entire schema, just the genre object id and genre name only are saved for the sub document for the sake of space complexity.

--- routes/movies.js
+++ routes/movies.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
 const { Movie, validate } = require('../models/movie');
+const {Genre} = require('../models/genre');
 
 router.get('/', async (req, res) => {
     const movies = await Movie.find()
-    .sort({ title: 1 })
-    .select('name');
+    .sort({ title: 1 });
 
     res.send(movies);
 });
@@ -21,13 +21,16 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', async (req, res) => {
     const result = validate(req.body);
-
     if(result.error) return res.status(400).send(result.error.details[0].message);
+
+    const genre = await Genre.findById(req.body.genreId);
+    if(!genre) return res.status(404).send('Could not find a genre with the id provided.');
 
     let movie = new Movie({
         title: req.body.title,
         genre: {
-            name: req.body.genre.name
+           _id: genre._id,
+           name: genre.name
         },
         numberInStock: req.body.numberInStock,
         dailyRentals: req.body.dailyRentals
@@ -40,14 +43,17 @@ router.post('/', async (req, res) => {
 
 router.put('/:id', async (req, res) => {
     const result = validate(req.body);
-
     if(result.error) return res.status(400).send(result.error.details[0].message);
   
+    const genre = await Genre.findById(req.body.genreId);
+    if(!genre) return res.status(404).send('Could not find a genre with the id provided.');
+
     const movie = await Movie.findByIdAndUpdate(req.params.id,
         {   
             title: req.body.title,
             genre: {
-                name: req.body.genre.name
+                _id: genre._id,
+                name: genre.name
             },
             numberInStock: req.body.numberInStock,
             dailyRentals: req.body.dailyRentals,
@@ -62,7 +68,7 @@ router.put('/:id', async (req, res) => {
 router.delete('/:id', async (req, res) => {
     const movie = await Movie.findByIdAndRemove(req.params.id);
 
-    if(!movie) return res.status(400).send('Could not find a movie with the id provided.');
+    if(!movie) return res.status(404).send('Could not find a movie with the id provided.');
 
     res.send(movie);
 });
