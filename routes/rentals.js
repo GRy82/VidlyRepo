2d7fef88c955bda968756747a226d2e7e4e46771
GRy82 <gwryan@buffalo.edu>
2021-04-26 16:39:34 -0400

    get and post route handlers created for rentals route file.

--- routes/rentals.js
+++ routes/rentals.js
@@ -4,3 +4,33 @@ const {Rental, validate} = require('../models/rental');
 const {Customer} = require('../models/customer');
 const {Movie} = require('../models/movie');
 
+router.get('/', async (req, res) => {
+    const rentals = await Rental.find().sort('movie.title');
+
+    res.send(rentals);
+});
+
+router.post('/', async (req, res) => {
+    const result = validate(req.body);
+    if(result.error) return res.status(400).send(result.error.details[0].message);
+
+    const movie = await Movie.findById(req.body.movieId);
+    if(!movie) return res.status(404).send('A movie could not be found with the id provided.');
+
+    const customer = await Customer.findById(req.body.movieId);
+    if(!customer) return res.status(404).send('A customer could not be found with the id provided.');
+
+    let rental = new Rental({
+        movie: {
+            _id: movie._id,
+            title: movie.title
+        },
+        customer: {
+            _id: customer._id,
+            name: customer.name
+        }
+    });
+
+    rental = await rental.save();
+    res.send(rental);
+});
