db2f5edca10bbdaed2e5009cac2f0496c94ed882
GRy82 <gwryan@buffalo.edu>
2021-04-26 17:04:33 -0400

    post handler of rentals endpoint updated to account for changes in schema.  The creation of a new rental object was then extracted to its own method. decrease in number of movies in stock also accounted for in the post route handler.

--- routes/rentals.js
+++ routes/rentals.js
@@ -20,17 +20,32 @@ router.post('/', async (req, res) => {
     const customer = await Customer.findById(req.body.movieId);
     if(!customer) return res.status(404).send('A customer could not be found with the id provided.');
 
-    let rental = new Rental({
+    let rental = produceRental(movie, customer, req.body);
+
+    rental = await rental.save();
+    await Movie.findByIdAndUpdate(movie._id, {
+        numberInSock: movie.numberInSock--
+    });
+
+    res.send(rental);
+});
+
+async function produceRental(movie, customer, rental){
+    return new Rental({
         movie: {
             _id: movie._id,
-            title: movie.title
+            title: movie.title,
+            dailyRentalRate: movie.dailyRentalRate
         },
         customer: {
             _id: customer._id,
-            name: customer.name
-        }
+            name: customer.name,
+            isGold: customer.isGold,
+            phone: customer.phone
+        },
+        dateOut: rental.dateOut,
+        dateReturned: rental.dateReturned,
+        rentalFee: rental.rentalFee
     });
 
-    rental = await rental.save();
-    res.send(rental);
-});
+}
