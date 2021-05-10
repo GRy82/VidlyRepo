9e86a307b0b9fe9edf45052d4e9522be7d92dced
Gregory Ryan <gwryan@buffalo.edu>
2021-05-10 17:08:31 -0400

    auth file created. Auth post request handler works to check for authenticity of the user trying ot log in. makes use of bcrpyt.

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const customers = require('./routes/customers');
 const movies = require('./routes/movies');
 const rentals = require('./routes/rentals');
 const users = require('./routes/users');
+const auth = require('./routes/auth');
 const home = require('./routes/home');
 const { mongo } = require('mongoose');
 const mongoose = require('mongoose');
@@ -31,6 +32,7 @@ app.use('/api/customers', customers);
 app.use('/api/movies', movies);
 app.use('/api/rentals', rentals);
 app.use('/api/users', users);
+app.use('/api/auth', auth);
 app.use('/', home);
 
 //if NODE_ENV is not defined, app.get returns dev env by default.
---
+++ routes/auth.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+const _ = require('lodash');
+const bcrypt = require('bcrypt');
+const { User, validate } = require('../models/user');
+
+//registers a user. 
+router.post('/', async (req, res) => {
+    const result = validate(req.body);
+    if(result.error) return res.status(400).send(result.error.details[0].message);
+
+    //Checks that the email is registered.
+    let user = await User.findOne({ email: req.body.email });
+    if(!user) return res.status(400).send('Invalid email or password');
+
+    //bcrypt will run entered pw through hash func, and add salt, then compare.
+    const validPassword = bcrypt.compare(req.body.password, user.password)
+    if(!validPassword) return res.status(400).send('Invalid email or password');
+
+    res.send(true);
+});
+
+function validate(req){
+    const schema = Joi.object({
+        email: Join.string().min(5).max(255).required().email(),
+        password: Joi.string().min(5).max(255).required()
+    });
+
+    return schema.validate(user);
+}
+
+module.exports = router;
