f185084e285a55f442a462321a4ce5fbb8818fbf
Gregory Ryan <gwryan@buffalo.edu>
2021-05-12 13:53:31 -0400

    Joi imported to auth.js. Notes added to auth.js in the form of comments. Notes are in reference to JSON Web Tokens/JWT

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const _ = require('lodash');
+const Joi = require('joi');
 const bcrypt = require('bcrypt');
 const { User, validate } = require('../models/user');
 
