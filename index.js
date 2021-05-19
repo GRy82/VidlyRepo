97866374768d0500f6f8615b215174026b9c95d0
Gregory Ryan <gwryan@buffalo.edu>
2021-05-19 14:11:26 -0400

    Error handling in index.js cleaned up to a more concise solution, with notes added as to how that works, and what the more convoluted alternative is.

--- index.js
+++ index.js
@@ -26,9 +26,21 @@ const mongoose = require('mongoose');
 
 //listens for uncaught exception on the level of node, ie. something not 
 //caught at a lower level. Remember that process is global object. 
-process.on('uncaughtException', (ex) => {
-    console.log('WE GOT AN UNCAUGHT EXCEPTION');
-    winston.error(ex.message, ex);
+//only works with synchronous code.
+// process.on('uncaughtException', (ex) => {
+//     winston.error(ex.message, ex);
+//     process.exit(1);
+// });
+
+winston.handleExceptions( //making new transport separate from that of default logger.
+    new winston.transports.File({ filename: 'uncaughtExceptions.log' }));
+//without this, async exception/promise rejection will not be logged.
+//because uncaughtException only works for synchronous processes.
+process.on('unhandledRejection', (ex) => {
+    //this will work with the uncommented code above. Winston will automatically catch it.
+    throw ex;
+    // winston.error(ex.message, ex);
+    // process.exit(1);
 });
 
 winston.add(winston.transports.File, { filename: 'logfile.log' });
@@ -37,7 +49,8 @@ winston.add(winston.transports.MongoDB, {
     level: 'info' //info, and higher priority, ie. info, warning, error.
 });
 
-throw new Error('Something failed during startup.');
+const p = Promise.reject(new Error('Failed miserably!'));
+p.then(() => console.log('Done'));
 
 if(!config.get('jwtPrivateKey')){
     console.error('FATAL ERROR: jwtPrivateKey is not defined.');
