40e42f5112ff9d17f01b39322190e264e5add901
Gregory Ryan <gwryan@buffalo.edu>
2021-05-19 10:44:45 -0400

    async middlewar wrapper removed from route handlers. express-async-errors now responsible for wrapping the route handlers at runtime. Logic can remain less convoluted as a result.

--- routes/genres.js
+++ routes/genres.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const asyncMiddleware = require('../middleware/async');
 const auth = require('../middleware/auth');
 const admin = require('../middleware/admin');
 const router = express.Router();
@@ -7,22 +6,22 @@ const mongoose = require('mongoose');
 const { Genre, validate } = require('../models/genre');
 
 
-router.get('/', asyncMiddleware(async (req, res) => {
+router.get('/', async (req, res) => {
    const genres = await Genre.find().sort('genreTitle');
    res.send(genres);
-}));
+});
 
-router.get('/:id', asyncMiddleware(async (req, res) => {
+router.get('/:id', async (req, res) => {
     const genre = await Genre.findById(req.params.id);
 
     if(!genre) 
         return res.status(404).send('A genre with that id does not exist.');
 
     res.send(genre);
-}));
+});
 
 
-router.post('/', auth, asyncMiddleware(async (req, res) => {
+router.post('/', auth, async (req, res) => {
     const result = validate(req.body);
     if(result.error)
        return res.status(400).send(result.error.details[0].message);
@@ -31,9 +30,9 @@ router.post('/', auth, asyncMiddleware(async (req, res) => {
     genre = await genre.save();
 
     res.send(genre);
-}));
+});
 
-router.put('/:id', asyncMiddleware(async (req, res) => {
+router.put('/:id', async (req, res) => {
     const result = validate(req.body);
     if(result.error) 
         return res.status(400).send(result.error.details[0].message);
@@ -46,7 +45,7 @@ router.put('/:id', asyncMiddleware(async (req, res) => {
         return res.status(404).send('A genre with that id does not exist.');
 
     res.send(genre);
-}));
+});
 
 //middleware functions passed as array. if auth is verified, and admin is verified
 //then the route handler will be executed. Otherwise, it does not execute.
