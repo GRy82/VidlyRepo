cd3b64b64cff35c77672dfa33a22706aa54ee867
Gregory Ryan <gwryan@buffalo.edu>
2021-05-24 19:15:13 -0400

    get request by id test suite created, and a single test created for it to check that single genre can be retrieved from test db.

--- tests/integration/genres.test.js
+++ tests/integration/genres.test.js
@@ -1,6 +1,7 @@
 //resquest is a function
 // it can be sent to various endpoints.
 const request = require('supertest');
+const {Genre} = require('../../models/genre');
 let server;
 
 describe('/api/genres', () => {
@@ -8,11 +9,32 @@ describe('/api/genres', () => {
     //callback within serves to reload the server.
     beforeEach(() => { server = require('../../index'); })
     //afterward, close the server.
-    afterEach(() => { server.close(); });
+    afterEach(async () => {
+        server.close();
+        await Genre.remove({});//this empty option removes all.
+     });
     describe('GET /', () => {
         it('should return all genres', async () => {
+            await Genre.collection.insertMany([
+                { genreTitle: 'genre1'},
+                { genreTitle: 'genre2' }
+            ])
             const res = await request(server).get('/api/genres');
             expect(res.status).toBe(200);
+            expect(res.body.length).toBe(2);
+            //.some is an array method. Pass it a predicate.
+            expect(res.body.some(g => g.genreTitle === 'genre1')).toBeTruthy();
+            expect(res.body.some(g => g.genreTitle === 'genre2')).toBeTruthy();
+        });
+    });
+    describe('GET /:id', () => {
+        it('should return genre with the given id.', async () => {
+            const genre = new Genre({ genreTitle: 'titleOne' });
+            await genre.save();
+            const res = await request(server).get('/api/genres/' + genre._id);
+
+            expect(res.status).toBe(200);
+            expect(res.body).toHaveProperty('genreTitle', genre.genreTitle);
         });
     });
 });
