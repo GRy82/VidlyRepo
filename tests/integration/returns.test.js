9540dadd9dba4d5c135162dd9ebac60dda83c691
Gregory Ryan <gwryan@buffalo.edu>
2021-06-15 14:27:33 -0400

    Returns integration tests refactored to eliminate repetitive code.

--- tests/integration/returns.test.js
+++ tests/integration/returns.test.js
@@ -9,52 +9,57 @@ describe('/api/returns', () => {
     let customerId;
     let movieId;
     let rental;
+    let token;
 
     beforeEach(async () => {
         server = require('../../index');
 
+        token = new User().generateAuthToken();
+        customerId = mongoose.Types.ObjectId();
+        movieId = mongoose.Types.ObjectId();
+
         rental = new Rental({
             customer: {
+                _id: customerId,
                 name: '12345',
                 phone: '12345'
             },
             movie: {
+                _id: movieId,
                 title: '12345',
                 dailyRentalRate: 2
             }
         });
         await rental.save();
-        customerId = rental.customer._id;
-        movieId = rental.movie._id;
 
     });
     afterEach(async () => {
         await Rental.remove({});
         await server.close();
     });
+
+    const exec = async () => {
+        return await request(server)
+            .post('/api/returns')
+            .set('x-auth-token', token)
+            .send({ customerId, movieId });
+    };
     
     it('should return 401 if client is not logged in!', async () => {
-        const res = await request(server)
-            .post('/api/returns')
-            .send({ customerId, movieId });
+        token = '';
+        const res = await exec();
 
         expect(res.status).toBe(401);
     });
     it('should return 400 if customerId is not present', async () => {
-        token = new User().generateAuthToken();
-        const res = await request(server)
-            .post('/api/returns')
-            .set('x-auth-token', token)
-            .send({ movieId });
+        customerId = '';
+        const res = await exec();
 
         expect(res.status).toBe(400);
     });
     it('should return 400 if movieId is not present', async () => {
-        token = new User().generateAuthToken();
-        const res = await request(server)
-            .post('/api/returns')
-            .set('x-auth-token', token)
-            .send({ customerId });
+        movieId = '';
+        const res = await exec();
 
         expect(res.status).toBe(400);
     });
