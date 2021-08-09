b3d1520e1617d653ed48282636bbdf347cb401e2
Gregory Ryan <gwryan@buffalo.edu>
2021-08-09 18:06:25 -0400

    seed.js file taken from Mosh repository.

--- routes/auth.js
+++ routes/auth.js
@@ -28,7 +28,7 @@ router.post('/', async (req, res) => {
 
 function validate(req){
     const schema = Joi.object({
-        email: Join.string().min(5).max(255).required().email(),
+        email: Joi.string().min(5).max(255).required().email(),
         password: Joi.string().min(5).max(255).required()
     });
 
